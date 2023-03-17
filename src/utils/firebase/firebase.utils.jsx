1022405d53a906a23c1c7a64c123fc03838c657e
Andrés Ferrer <128063946+AndresFerrerPestana@users.noreply.github.com>
2023-03-17 16:40:56 +0000

    Added our sign in component as well as firebase utils and brought in firebase libraries

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,3 +32,35 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  //   console.log(userSnapshot.exists());
+
+  // if user data does not exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    // create / set the document with data from userAuth in my collection
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      //   console.log("error creating the user", error.message);
+    }
+  }
+
+  // if user data exists
+  return userDocRef;
+
+  // return userDocRf
+};
